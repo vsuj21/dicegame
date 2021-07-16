d3f471e32c653bd3a6588f729d12934ac72338cf
vsuj21 <86345373+vsuj21@users.noreply.github.com>
2021-07-16 11:50:42 +0530

    game

---
+++ index.js
@@ -0,0 +1,29 @@
+var randomNumber1= Math.floor(Math.random()*6)+1;
+var randomdiceimage= "dice" + randomNumber1 + ".png"
+
+var randomimagesource = "images/" + randomdiceimage;
+
+var image1 = document.querySelectorAll("img")[0];
+
+image1.setAttribute("src",randomimagesource);
+
+var randomNumber2= Math.floor(Math.random()*6)+1;
+var randomdiceimage2= "dice" + randomNumber2 + ".png"
+var randomimagesource2 = "images/" + randomdiceimage2;
+
+var image2 = document.querySelectorAll("img")[1];
+
+image2.setAttribute("src", randomimagesource2);
+
+
+
+if (randomNumber1>randomNumber2) {
+	document.querySelector("h1").innerHTML="player1 wins!⛳";
+}
+else if (randomNumber2>randomNumber1) {
+	document.querySelector("h1").innerHTML = "player2 wins!⛳";
+}
+else{
+	document.querySelector("h1").innerHTML = "Draw!"
+}
+
